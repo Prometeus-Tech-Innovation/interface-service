c921dd1646b4a5aac94a7aaf4b02acb589866fa6
vithorcamara <vitor.camarapd@gmail.com>
2025-05-12 17:29:08 -0300

    Fix: BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SplashScreen from '../src/pages/SplashScreen'
-import Login from '../src/pages/LoginPage'
-import Home from '../src/pages/HomePage'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SplashScreen from '../src/pages/SplashScreen';
+import Login from '../src/pages/LoginPage';
+import Home from '../src/pages/HomePage';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-            <Routes>
-                <Route path='/' element={<SplashScreen/>} />
-                <Route path='/login' element={<Login/>} />
-                <Route path='/home' element={<Home/>} />
-            </Routes>
-        </Router>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SplashScreen />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
